a6893dd54e5dea2bca2943f46e44df8281d142d8
bl <bl41@rice.edu>
2024-02-19 11:51:24 -0600

    tested

--- README.md
+++ README.md
@@ -7,13 +7,15 @@ Boning Li, Timofey Efimov, Abhishek Kumar, Jose Cortes, Gunjan Verma, Ananthram 
 
 ## organization
 
+Uncompress `./dataset.zip`, which contains an example dataset (regular grid wifi topology). The organization of code is as follows:
+
 ```
 ├── dataset/
-│   ├── ns3-simulation-codes/
+│   ├──[NOT HERE YET, TO BE ADDED] ns3-simulation-codes/
 │   │   ├── data.ipynb                       # for data processing
 │   │   ├── py_helpers.ipynb                 # for automating ns3 args
 │   │   └── wireless-traffic-to-kpi5-ext.cc  # c++ code for ns3 simulation
-│   ├── WifiGridFtR1.99S1St180Dr50Le2F4/     # datasets
+│   ├── WifiGridR1S2St180Dr50Le2F4/
 │   └── ...
 ├── configs/
 │   ├── wifigrid.ini
@@ -26,13 +28,21 @@ Boning Li, Timofey Efimov, Abhishek Kumar, Jose Cortes, Gunjan Verma, Ananthram 
 │   ├── graph_convs.py  # customized gnn modules
 │   └── datagen.py      # handling network data and KPIs label
 ├── train_univ_emb_batch.py   # main entrance for training
-├── main-sow.ipynb            # for automating training args
+├── main-sow.ipynb            # for automating training script args
 └── main-harvest.ipynb        # for processing and visualizing results
 ```
 
 ## example
 
-Run the training script
+If you also use Docker, try pulling [this image](https://hub.docker.com/r/bl41/digtwin) by
+```
+$ docker pull bl41/digtwin
+```
+and run a container:
+```
+$ docker run --rm -it --name CONTAINER_NAME -v PATH_TO_CLONED_REPO:/root/digital-twin/wireless_digital_twin/ bl41/digtwin:digtwin-py /bin/bash
+```
+In the container, cd to the projected `/root/digital-twin/wireless_digital_twin/` and run the training script:
 ```
 $ python train_univ_emb_batch.py -c ./configs/wifigrid.ini -m PlanNetEmbGCNBatch --loss mse -ep 100 -f 1
 ```
@@ -42,6 +52,10 @@ $ python train_univ_emb_batch.py -c ./configs/wifigrid.ini -m PlanNetEmbGCNBatch
 - `--loss mse` : use MSE loss
 - `-ep 100` : train for 100 epochs
 - `-f 1` : cross-validation fold#1
+
+This should start the training process\*. 
+
+> \*Although this will be using CPUs, the code itself should have no issue running on GPUs (tested on a Ubuntu server). It's just because my Docker image has a CPU-version tensorflow (which is because my pc on which I'm compiling this documentation and making sure this thing can run on a different platform doesn't have GPUs :\
 
 ## configs
 
@@ -55,19 +69,22 @@ label_scale     = True    # if False, use raw label values; if True, apply robus
 batching        = True    # if False, train one instance at a time (the batch_size set above will be ignored)
 batch_norm      = False   # (not effective yet)
 layer_norm      = False   # (not effective yet)
-add_noise       = False   # if True, add gaussian noise to input (for random-flow experiments)
+add_noise       = False   # if True, add gaussian noise to input (for random-flow experiments only)
 ```
 
 ## model
 
-We have implemented learnable digital twin architectures @[./utils/models.py](./utils/models.py) with the following features enabled:<br>
+We have implemented learnable digital twin architectures in [./utils/models.py](./utils/models.py) with the following features enabled:<br>
 e.g. [PlanNetEmbGCNBatch](./utils/models.py#L484)
 
 - **Multi-task learning**: shared embedding layers and individual readout layers, enabling joint learning of multiple KPIs and transfer learning using new KPI(s).
-- **Parameter sharing**: the trainable parameters are identical across different layers for $t=1,...,T$, serving as a regularization against over-parameterization.
-- **Batch training**:
-- **Flexible node-embedding module**:
 
+- **Parameter sharing**: identical trainable parameters are across different layers for $t=1,...,T$, serving as a regularization against over-parameterization.
+
+- **Batch training\***: if enabled, the node, link, and path indices will be manipulated so that it works as if instances in a batch are on the same concatenated graph. Implementation details start at [line#242@./utils/models.py](./utils/models.py#L242).
+> \* Batch or layer normalization actually would degrade the performance according to my observation. Until I figure out the correct way of doing it, let's have both `batch_norm` and `layer_norm` set to `False` in the config file.
+
+- **Flexible node-embedding module**: currently, two types of graph convolutions (graph convolution - GCN and edge conditional convolution - ECC) are available in [./utils/graph_convs.py](./utils/graph_convs.py). We can have more customized architectures for sure.
 
 
 ## TODOs
--- train_univ_emb_batch.py
+++ train_univ_emb_batch.py
@@ -34,7 +34,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 
 if __name__=="__main__":
-    
+
     # Instantiate the parser
     parser = argparse.ArgumentParser()
     parser.add_argument('-c','--config', type=str, default='./configs/config.ini')
@@ -43,16 +43,16 @@ if __name__=="__main__":
     parser.add_argument('--loss', type=str, default='mse')
     parser.add_argument('--optm', type=str, default='adam')
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--suffix', type=str, default='xl2')
-    
+    parser.add_argument('--suffix', type=str, default='')
+
     parser.add_argument('--freeze', action='store_true')
     parser.add_argument('-lw','--loadweights', type=str, default='')
     parser.add_argument('-es','--early_stop', type=int, default=0)
     parser.add_argument('-ep','--epochs', type=int, default=0)
-    
+
     parser.add_argument('--resume', action='store_true')
     args = parser.parse_args()
-    
+
     ##
     cfg_file = args.config
     print(cfg_file)
@@ -60,14 +60,14 @@ if __name__=="__main__":
     if args.cvfold != 0:
         config['Paths']['logs'][0] += f'/cv{args.cvfold}/'
     h_params = dict(**config['GNN'], **config['LearningParams'])
-    
+
     ##
-    n_epochs = int(h_params['epochs']) if not args.epochs else args.epochs 
+    n_epochs = int(h_params['epochs']) if not args.epochs else args.epochs
 
     ##
     print(config)
     datagens, datasets = dg.get_data_gens_sets(config, fold=args.cvfold)
-        
+
     ##
     initial_learning_rate = float(h_params['learning_rate'])
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
@@ -83,42 +83,42 @@ if __name__=="__main__":
         optimizer = tf.keras.optimizers.SGD(learning_rate=initial_learning_rate)
     else:
         raise
-        
+
     m_argkw = {
         'final_activation': config['Paths']['fin_activ'][0] if 'fin_activ' in config['Paths'] else None,
-        'train_on': config['Paths']['trainon'], 
+        'train_on': config['Paths']['trainon'],
         'loss': args.loss,
         'sharing': h_params['sharing'] if 'sharing' in h_params else True,
         'add_noise': h_params['add_noise'] if 'add_noise' in h_params else False,
     }
-    
+
     if args.freeze:
         h_params['learn_embedding'] = False
-    
+
     ModelClass = globals()[args.model]
     #batching = 'batch' in args.model.lower()
     batching = h_params['batching'] and (h_params['batch_size']>0)
     if batching:
         m_argkw['batching'] = True
         m_argkw['batch_norm'] = h_params['batch_norm'] and (h_params['batch_size']>1)
-        
-    model = ModelClass(h_params, **m_argkw)    
+
+    model = ModelClass(h_params, **m_argkw)
     model.build()
-    
-    # transfer learning    
+
+    # transfer learning
     if args.freeze:
         model.path_update.trainable = False
         model.edge_update.trainable = False
-        model.node_update.trainable = False      
-    
+        model.node_update.trainable = False
+
     model.compile(
-        optimizer   = optimizer, 
+        optimizer   = optimizer,
         loss = m_argkw['loss'],
         run_eagerly = args.debug
     )
     #print(uf.get_layers_weights(model)['path_update'])
-    
-    print('len(model.trainable_weights) =', len(model.trainable_weights))    
+
+    print('len(model.trainable_weights) =', len(model.trainable_weights))
     if args.loadweights:
         print()
         load_ini_file = glob.glob(os.path.join(os.path.dirname(args.loadweights), '*.ini'))[0]
@@ -127,64 +127,64 @@ if __name__=="__main__":
         load_model.build()
         load_model.compile(optimizer, loss=m_argkw['loss'])
         #load_model(datagens['train'].__getitem__(0)[0])
-        
+
         print('Loading pretrained weights from', args.loadweights)
         ckpt = tf.train.Checkpoint(load_model)
         ckpt.restore(args.loadweights).expect_partial()
-        
+
         model.path_update.set_weights(load_model.path_update.get_weights())
         model.edge_update.set_weights(load_model.edge_update.get_weights())
         model.node_update.set_weights(load_model.node_update.get_weights())
         for lab in model.train_on:
             if lab in list(load_model.readout):
                 print(lab, ': loading readout layers...')
-                model.readout[lab].set_weights(load_model.readout[lab].get_weights())   
+                model.readout[lab].set_weights(load_model.readout[lab].get_weights())
             if lab in list(load_model.final):
                 print(lab, ': loading final layer...')
-                model.final[lab].set_weights(load_model.final[lab].get_weights())   
-                
+                model.final[lab].set_weights(load_model.final[lab].get_weights())
+
     mclass = re.findall('\'.+\..+\.(.+)\'', str(model.__class__))[0] #<class 'utils.models.PlanNetEmb'>
     print()
-    
+
     # save model
     model_dirname = 'TRG+' + '+'.join(model.train_on) + '_DS+' + '+'.join(
         [os.path.basename(d) for d in config['Paths']['data']]
     )
     print(model_dirname)
-    
+
     log_dir = os.path.join(
-        config['Paths']['logs'][0], 
-        args.model, 
+        config['Paths']['logs'][0],
+        args.model,
         model_dirname+f'_LR{initial_learning_rate:.2e}'
     )
-    if batching: 
+    if batching:
         log_dir += '_BS'+str(h_params['batch_size'])
     if not m_argkw['sharing']:  #whether to share parameters accross layers
         log_dir += '_noShare'
-        
+
     log_dir += '_LF+'+args.loss #loss function
-    
+
     if 'add_noise' in m_argkw and m_argkw['add_noise']:
         log_dir += '_wNoise'
-    
+
     log_dir += f'_{args.suffix}'
     os.makedirs(log_dir, exist_ok = True)
     try:
         shutil.copyfile(
-            cfg_file, 
+            cfg_file,
             os.path.join(log_dir, os.path.basename(cfg_file))
         )
     except shutil.SameFileError:
         pass
-    
+
     # callback and checkpoint settings
     total_tr, total_va = datagens['train'].n, datagens['validate'].n
     iters_per_epoch   = total_tr if not batching else -(-total_tr//model.batch_size)
     cb_csvlogger_path = os.path.join(log_dir, 'training.log')
     cb_ckpt_at_path   = lambda x: os.path.join(log_dir, f'cp-{x:04d}.ckpt')
     cb_latest_path    = os.path.join(log_dir, "cp-latest.ckpt")
-    cb_best_paths     = lambda x: os.path.join(log_dir, f'cp-best-{x}.ckpt')    
-    
+    cb_best_paths     = lambda x: os.path.join(log_dir, f'cp-best-{x}.ckpt')
+
 
     """
     CUDA_VISIBLE_DEVICES=-1 python train_univ.py --suffix x_debug_retrain_2 -c ./configs/nsf-delay-jitter-throughput-drops-dr100.ini -m PlanNet -f 1 --loadweights ./output_j/cv1/PlanNet/TRG+delay+jitter_DS+NsfnetR1S1St180Dr100_LR5.00e-04_x_debug_/cp-0100.ckpt
@@ -195,9 +195,9 @@ if __name__=="__main__":
         print('Loading weights from', cb_latest_path)
         ckpt = tf.train.Checkpoint(model)
         ckpt.restore(cb_latest_path).expect_partial()
-        initial_epoch = model.optimizer.iterations.numpy() // iters_per_epoch  
+        initial_epoch = model.optimizer.iterations.numpy() // iters_per_epoch
         print('Resuming from epoch', initial_epoch, model.optimizer.iterations.numpy(), iters_per_epoch )
-        
+
     print('len(model.trainable_weights) =', len(model.trainable_weights))
     for block in model.layers:
         print(block.name, block.trainable)
@@ -206,7 +206,7 @@ if __name__=="__main__":
                 print('\t', layer.name, layer.trainable)
         except:
             pass
-            
+
     print()
     train_ds = datasets['train'].shuffle(total_tr//3, reshuffle_each_iteration=True)
     valid_ds = datasets['validate']#.take(model.batch_size)
@@ -214,8 +214,8 @@ if __name__=="__main__":
         train_ds = dg.dataset_batchify(train_ds, model.batch_size, padding=True)
         valid_ds = dg.dataset_batchify(valid_ds, model.batch_size, padding=True)
     else:
-        valid_ds = valid_ds.take(1) 
-  
+        valid_ds = valid_ds.take(1)
+
 
     ################################
     #    MY TRAINING FRAMEWORK     #
@@ -243,14 +243,14 @@ if __name__=="__main__":
                     f"Train: epoch {ep:3d}, step {step:4d}, Loss: {train_log['loss']/(step+1):8f}"
                 )
 
-            # average for the current epoch 
+            # average for the current epoch
             train_log = {
-                **{'epoch': ep}, 
+                **{'epoch': ep},
                 **{k:v.numpy() / (step+1) for k,v in train_log.items()}
             }
-            
+
             # checkpoints
-            model.save_weights(cb_ckpt_at_path(ep+1))            
+            model.save_weights(cb_ckpt_at_path(ep+1))
 
             # validation
             valid_log = {}
@@ -262,10 +262,10 @@ if __name__=="__main__":
                     valid_log = log
                 else:
                     for k in log.keys():
-                        valid_log[k] += log[k]  
+                        valid_log[k] += log[k]
                 pbar.set_description(
                     f"Valid: epoch {ep:3d}, step {step:4d}, Loss: {valid_log['loss']/(step+1):8f}"
-                )                        
+                )
             # average
             valid_log = {'val_'+k : v.numpy() / (step+1) for k,v in valid_log.items()}
             print("Validation MAE Loss is: {}".format(valid_log['val_mae']))
@@ -276,12 +276,12 @@ if __name__=="__main__":
             # save to csv
             df_csv = pd.DataFrame([learning_log[-1]])
             df_csv.to_csv(
-                cb_csvlogger_path, 
-                index=False, 
-                mode='a', 
-                header=not os.path.exists(cb_csvlogger_path) 
-            ) # convert DataFrame to csv   
-            
+                cb_csvlogger_path,
+                index=False,
+                mode='a',
+                header=not os.path.exists(cb_csvlogger_path)
+            ) # convert DataFrame to csv
+
             # best model and early stop
             esincrement = 0
             for mi, m in enumerate(['loss', 'mae']):
@@ -298,7 +298,7 @@ if __name__=="__main__":
             escount += (-escount) ** (esincrement<mi+1) #increment or reset
 
             model.save_weights(cb_latest_path)
-                    
+
     except KeyboardInterrupt:
         print('exit and saved to', cb_latest_path)
         model.save_weights(cb_latest_path)
